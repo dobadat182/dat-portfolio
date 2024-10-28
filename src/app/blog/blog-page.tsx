7f2d7b499e98559ec55b6be2eb9e0ab0da953374
dat <badat@vietnambooking.com>
2024-10-29 00:21:18 +0700

    Update

--- src/app/blog/blog-page.tsx
+++ src/app/blog/blog-page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { PostCard } from "@/lib/interface";
 import { Suspense } from "react";
---
+++ src/app/blog/loading.tsx
@@ -0,0 +1,9 @@
+const loading = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      Đang tải...
+    </div>
+  );
+};
+
+export default loading;
